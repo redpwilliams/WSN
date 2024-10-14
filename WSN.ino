e17539c3db6fe213a4fee7e5e2e7a31a6cb9cb14
Red Williams <redpwilliams@gmail.com>
2024-10-14 15:50:21 -0400

    added more comments, started boost converter output calc function

--- WSN.ino
+++ WSN.ino
@@ -1,55 +1,73 @@
 // Reference: https://github.com/T-622/25KHz-Arduino-Fan-PWM-Control/blob/main/25KHz_PWM_UNO.ino
 
+/* 
+  TODOs:
+    1. Reset duty cycle to 0 if there is no input voltage to the ADC,
+        Meaning, the boost converter is not operating. Don't want to see
+        huge voltage spikes when reconnecting the load and the duty cycle is already high
+    2. Create custom debug logger object. Should be able to, anywhere in the code, set a debug
+       level (info, warn, error, etc) and log it to the serial monitor
+       
+*/
+
 /*
-  Extra notes & consideration:
-  - Throughout this code, the byte primitive is used more
-    often than ints because ints are 2 bytes wide.
+  REVIEWs:
+    1. Refer to https://www.baldengineer.com/5-voltage-divider-circuits.html
+        Specifically, the part on adding transistors to divider circuits. To prevent leakage into the
+        Arduino pin, we can use another pin to turn off that voltage divider circuit when we aren't
+        reading from it, saving power.
 */
 
 // Custom types for readability
-typedef const byte Pin_t; // For arduino in/out pins
-typedef int Voltage_t;
-typedef const int Resistance_t;
-typedef byte DutyCycle_t; // As whole ints (30% duty cycle: DutyCycle_t dc = 30;)
+typedef const byte Pin_t;       // Arduino in/out pins
+typedef const float Voltage_t;  // Constant & variable/measured voltage sources
+typedef const int Resistance_t; // Resistors
+typedef const float Current_t;  // Current (mainly for calculations)
+typedef byte DutyCycle_t;       // As whole ints (30% duty cycle: DutyCycle_t dc = 30;)
 
-// TODO: Pin definitions
-// Pin_t PIN_Thermistor = x;
-Pin_t PIN_CUTOFF = A0;
+// TODO: Analog/Input Pin definitions
+// Pin_t PIN_Thermistor = AX;
+Pin_t PIN_BOOST_REF = A0;   // Connects to R6 node in voltage divider/ADC sub circuit
 
 // Timer parameters
-const unsigned long PWM_FREQ_HZ = 50e3; // Adjust this value to adjust the frequency
-const unsigned long TCNT1_TOP = 16000000/(2*PWM_FREQ_HZ);
-Pin_t OC1A_PIN = 9; // Use arduino pin 9 for pwm signal
+const unsigned long PWM_FREQ_HZ = 50e3;               // Switching frequency
+const unsigned long TCNT1_TOP = 16e6/(2*PWM_FREQ_HZ); // Sets up timer
+Pin_t PIN_PWM = 9;                                    // Arduino pin for pwm signal
 
 // Duty cycle info
 DutyCycle_t currentDutyCycle = 0; // Start at 0%
-const DutyCycle_t MAX_DC = 60;
+const DutyCycle_t MAX_DC = 60;    // Acts as a software-based kill switch
 
 // Circuit components (reference schematic)
-Resistance_t R5 = 780;
-Resistance_t R6 = 1000;
-const Voltage_t D1 = 0.206; // Measure with multimeter
-const Voltage_t D5 = 0.580; // Measure with multimeter
-const Voltage_t D3 = 0.580; // Measure with multimeter
+Resistance_t R5 = 780;  // ADC voltage divider
+Resistance_t R6 = 1000; // ADC voltage divider
+const Voltage_t D1 = 0.206f; // Measure with multimeter
+const Voltage_t D5 = 0.580f; // Measure with multimeter
+const Voltage_t D3 = 0.580f; // Measure with multimeter
 
 // Circuit parameters
-const Voltage_t NOMINAL_BATTERY = 9;
-const Voltage_t V_INPUT = 5;
-const Voltage_t V_OUTPUT = 9;
+const Voltage_t NOMINAL_BATTERY = 9.0f;   // What the battery voltage should be
+const Voltage_t BOOST_STD_INPUT = 5.0f;   // What the input of the boost converter should be
+const Voltage_t BOOST_STD_OUTPUT = 10.4f; // What the output of the boost converter should be
 
-// Analog Input (to read battery voltage)
-
+/// Runs once at 
 void setup() {
-  pinMode(PIN_CUTOFF, INPUT);
+  // Configure the voltage divider as an input
+  pinMode(PIN_BOOST_REF, INPUT);
+
+  // Enable the timer to drive the 50 kHz signal
   configureTimer();
-  // configureDutyCycle();
+
+  // Applies the starting duty cycle to the PWM signal
   setPWM_DutyCycle(currentDutyCycle);
 }
 
 void loop() {
-
   // Calculate actual battery voltage
   Voltage_t reference = calculateBatteryVoltage();
+
+  // TODO #1
+
   DutyCycle_t nextDutyCycle;
 
   if (reference < NOMINAL_BATTERY) {
@@ -72,7 +90,7 @@ void loop() {
 }
 
 void configureTimer() {
-  pinMode(OC1A_PIN, OUTPUT);
+  pinMode(PIN_PWM, OUTPUT);
 
   // Clear Timer1 control and count registers
   TCCR1A = 0;
@@ -84,17 +102,31 @@ void configureTimer() {
   ICR1 = TCNT1_TOP;
 }
 
-// Reads 
+/// Reads 
 void configureDutyCycle() {
   // Bias to further vary output voltage
   // More positive bias = higher duty cycle = higher output voltage
-  const Voltage_t bias = -100e-3; // 100mV bias
-  currentDutyCycle = 1 - (V_INPUT / (V_OUTPUT + D1 + bias));// REVIEW - Only considers Schottky diode drop
+  Voltage_t bias = -100e-3; // 100mV bias
+  currentDutyCycle = 1 - (BOOST_STD_INPUT / (BOOST_STD_OUTPUT + D1 + bias));// REVIEW - Only considers Schottky diode drop
   currentDutyCycle *= 100; // Convert from decimal to percentage
 }
 
+/// Returns the output to the Boost Converter
+Voltage_t measureBoostVoltage() {
+
+  // Read the reference voltage. Resistors chosen such that this input is always less than 5V
+  Voltage_t boostReference = analogRead(PIN_BOOST_REF) * 5f / 1023.0f;
+
+  // Calculate current
+  
+  return voltageRead * (R5 + R6) / R6; // TODO: Account for D3 diode drop
+}
+
+// DEPRECATED
 Voltage_t calculateBatteryVoltage() {
-  const Voltage_t voltageRead = analogRead(PIN_CUTOFF) * 5 / 1023.0;
+  
+
+  // Calculate current through
   return voltageRead * (R5 + R6) / R6; // TODO: Account for D3 diode drop
 }
 
